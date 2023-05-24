e6815210c40a3117621f748a75dcdda4c43cc38b
Zakaria <zakariaprofes@gmail.com>
2023-05-24 10:31:55 +0200

    Added Visualization, Demo and updated few methods

--- MinimalGrad/core.py
+++ MinimalGrad/core.py
@@ -3,13 +3,14 @@ from numba import njit
 
 
 class Tensor:
-    def __init__(self, data, requires_grad=False, _children=(), _dtype=np.float32):
+    def __init__(self, data, requires_grad=False, _children=(), _op='',_dtype=np.float32):
         self.data = np.array(data, dtype=_dtype)
         self.requires_grad = requires_grad
         self.grad = 0
         self._backward = lambda: None
         self._prev = set(_children)
         self.dim = self.data.ndim
+        self._op = _op  # the op that produced this node, for graphviz
 
     def to_array(self):
         return self.data
@@ -46,18 +47,19 @@ class Tensor:
         return self.data.shape
 
     def sum(self, axis=None):
-        return Tensor(np.sum(self.data, axis=axis, dtype=self.data.dtype), requires_grad=self.requires_grad)
+        return Tensor(np.sum(self.data, axis=axis, dtype=self.data.dtype), requires_grad=self.requires_grad, _op='Sum')
 
     def mean(self, axis=None):
-        return Tensor(np.mean(self.data, axis=axis, dtype=self.data.dtype), requires_grad=self.requires_grad)
+        return Tensor(np.mean(self.data, axis=axis, dtype=self.data.dtype), requires_grad=self.requires_grad,
+                      _op='Mean')
 
     def transpose(self, axes=None):
-        return Tensor(np.transpose(self.data, axes=axes), requires_grad=self.requires_grad)
+        return Tensor(np.transpose(self.data, axes=axes), requires_grad=self.requires_grad, _op='Transpose')
 
     def __add__(self, other):
         other = other if isinstance(other, Tensor) else Tensor(np.array(other, dtype=self.data.dtype))
         result = Tensor(self.data + other.data, requires_grad=self.requires_grad or other.requires_grad,
-                        _children=(self, other))
+                        _children=(self, other), _op='+')
 
         def _backward():
             self.grad += result.grad
@@ -70,7 +72,7 @@ class Tensor:
     def __mul__(self, other):
         other = other if isinstance(other, Tensor) else Tensor(np.array(other, dtype=self.data.dtype))
         result = Tensor(self.data * other.data, requires_grad=self.requires_grad or other.requires_grad,
-                        _children=(self, other))
+                        _children=(self, other), _op='*')
 
         def _backward():
             self.grad += other.data * result.grad
@@ -82,7 +84,7 @@ class Tensor:
 
     def __truediv__(self, other):
         other = other if isinstance(other, Tensor) else Tensor(np.array(other, dtype=self.data.dtype))
-        return self * other**-1
+        return self * other ** -1
 
     def __sub__(self, other):
         other = other if isinstance(other, Tensor) else Tensor(np.array(other, dtype=self.data.dtype))
@@ -93,14 +95,24 @@ class Tensor:
 
     def __pow__(self, power):
         assert isinstance(power, (int, float)), "Supporting only int and float power for now."
-        result = Tensor(self.data ** power, _children=(self, ), _dtype=self.data.dtype)
+        result = Tensor(self.data ** power, _children=(self,), _op="power", _dtype=self.data.dtype)
 
         def _backward():
-            self.grad += (power * self.data**(power-1)) * result.grad
+            self.grad += (power * self.data ** (power - 1)) * result.grad
 
         result._backward = _backward
 
         return result
+
+    def relu(self):
+        out = Tensor(0 if self.data < 0 else self.data, _children=(self,), _op='ReLU')
+
+        def _backward():
+            self.grad += (out.data > 0) * out.grad
+
+        out._backward = _backward
+
+        return out
 
     def __getitem__(self, index):
         return Tensor(self.data[index], requires_grad=self.requires_grad)
